fc2ba2d6a8c55450c45530756d0eb1be138b279b
kirya <cloudkserg11@gmail.com>
2018-04-13 19:22:05 +0300

    https://chronobank.atlassian.net/browse/MD-232 - waves - blockprocessor, add comments of license to models and factories, delete native driver

--- config/index.js
+++ config/index.js
@@ -2,6 +2,10 @@
  * Chronobank/eth-blockprocessor configuration
  * @module config
  * @returns {Object} Configuration
+ * 
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ * @author Kirill Sergeev <cloudkserg11@gmail.com>
  */
 
 require('dotenv').config();
--- models/blockModel.js
+++ models/blockModel.js
@@ -2,6 +2,11 @@
  * Mongoose model. Represents a block in eth
  * @module models/blockModel
  * @returns {Object} Mongoose model
+ *
+ * 
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ * @author Kirill Sergeev <cloudkserg11@gmail.com>
  */
 
 const mongoose = require('mongoose'),
